65ed175621a6b2a0018f245ada60cdb7b71c95cc
Trang Hoang <tranghoang081299@gmail.com>
2021-08-03 16:38:16 +0700

    Chapter 8: Basic Login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,7 @@ class UserController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "success_signup"
       redirect_to @user
     else
